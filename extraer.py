0819d9161dd525b2abfdbc5f4214d04a9f3d3505
lui358 <lui358@gmail.com>
2024-06-03 16:05:41 -0700

    Delete extraer.py

--- extraer.py
+++
@@ -1,9 +0,0 @@
-import pdfplumber
-
-def extraer_metadatos(pdf_path):
-    with pdfplumber.open(pdf_path) as pdf:
-        first_page = pdf.pages[0]
-        texto = first_page.extract_text()
-        # Asumimos que el título está en la primera línea
-        titulo = texto.split('\n')[0]
-        return titulo
